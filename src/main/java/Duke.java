ce66472d2c6c00573291c6a82f3d40301a88d94d
Yukino2711 <205627553+Yukino2711@users.noreply.github.com>
2026-08-17 13:39:23 +0800

    Level-1 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String separator = "____________________________________________________________";
@@ -10,7 +12,20 @@ public class Duke {
         System.out.println("Hello! I'm yqr.");
         System.out.println("What can I do for you?");
         System.out.println(separator);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(separator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String command = scanner.nextLine();
+            if (command.equals("bye")) {
+                System.out.println(separator);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(separator);
+                break;
+            }
+
+            System.out.println(separator);
+            System.out.println(command);
+            System.out.println(separator);
+        }
     }
 }
