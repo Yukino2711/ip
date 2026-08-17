51fba356793438156a82710f65bd392d19cbea3d
Yukino2711 <205627553+Yukino2711@users.noreply.github.com>
2026-08-17 13:28:18 +0800

    Level-0 done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,16 @@
 public class Duke {
     public static void main(String[] args) {
-        String banner = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String separator = "____________________________________________________________";
+        String banner = "+-------+\n"
+                + "|  yqr  |\n"
+                + "+-------+";
+
+        System.out.println(separator);
         System.out.println(banner);
+        System.out.println("Hello! I'm yqr.");
+        System.out.println("What can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
